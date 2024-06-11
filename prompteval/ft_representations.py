eafe36d8fb059accd465d7e7daf794932366257d
LucWeber <lucasweber000@gmail.com>
2024-06-11 20:27:08 +0200

    Refactor model ft

--- prompteval/ft_representations.py
+++ prompteval/ft_representations.py
@@ -7,28 +7,24 @@ import pandas as pd
 import torch
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader, TensorDataset
-from transformers import AutoTokenizer
 
 from prompteval.utils.utils_ft import (
-    MultiLabelRaschModel,
     MultiLabelRaschModel_ID_tokens,
-    OneHotRaschModel,
+    ModelConfig,
+    n_examples_mmlu,
     build_feature_tensor,
     build_label_tensor,
     get_args,
-    get_examples_per_task_mmlu,
     get_save_name,
     process_text,
-    task_order_mmlu,
     train_model,
+    n_max_tasks
 )
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 np.random.seed(42)
 
 # TODO:
-# 1. change save names according to create_data.py
-# 2. change the way that the model is saved
 # 3. generalise training to potential new benches -> standardise
 # 4. refactor code
 
@@ -37,71 +33,37 @@ BASE_PATH = "./"
 # Data hyperparameters
 TRAIN_SPLIT_LLMS = "odds"
 TRAIN_SPLIT_SIZE = 0.8
-MAX_LENGTH = 200  # max length of tokenizer output
+MAX_LENGTH = 300  # max length of tokenizer output
 
-ALT_SCHEDULER = False
-CLS = False
-ID_TOKEN = True
+# Model hyperparameters
+D = 25 # size of used latent representation space
+CLS = False 
 
 args = get_args()
 print(args)
 
 mmlu = args.bench == "MMLU"
 
-# Selecting task to train
-if not mmlu:
-    tasks = [
-        s.replace(".csv", "") for s in [x[2] for x in os.walk(os.path.join(BASE_PATH, "templates", args.bench))][0]
-    ]
-    tasks.sort()
-else:
-    tasks = task_order_mmlu
-
-tasks = tasks[: args.n_tasks]
+# Selecting tasks to train
+n_tasks = n_max_tasks[args.bench] if args.n_tasks == None else args.n_tasks
+tasks = [s.replace(".csv", "") for s in [x[2] for x in os.walk(os.path.join(BASE_PATH, "data", "templates", args.bench))][0]]
+tasks.sort()
+tasks = tasks[:n_tasks]
 print(tasks)
 
-mmlu_data_dir = "../data/mmlu/mmlu_output_codellama_codellama-34b-instruct/t_0"
-
-n_examples_mmlu = get_examples_per_task_mmlu(mmlu_data_dir, tasks)
-n_example_tokens = 100 * args.n_tasks if not mmlu else sum(n_examples_mmlu)
-
-if args.model_name != "OneHotRaschModel":
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-    if ID_TOKEN:
-        example_tokens = [f"[Example_{i}]" for i in range(0, n_example_tokens)]
-        num_added_tokens = tokenizer.add_tokens(["[INPUT]", "[BR]", "[DATA_ID]"] + example_tokens)
-        example_token_lookup = {
-            n_example: token
-            for n_example, token in zip(range(0, n_example_tokens), tokenizer.convert_tokens_to_ids(example_tokens))
-        }
-        example_token_lookup.update({"[DATA_ID]": tokenizer.convert_tokens_to_ids("[DATA_ID]")})
-    else:
-        num_added_tokens = tokenizer.add_tokens(["[INPUT]", "[BR]"])
-        example_token_lookup = None
-else:
-    tokenizer = None
-
-
-# Loading and encoding format templates
+# Loading and encoding format templates; count them
 formats = []
 n_examples = []
 n_formats = []
 for i, task in enumerate(tasks):
-    formats_df = pd.read_csv(os.path.join(BASE_PATH, "templates", args.bench, f"{task}.csv"))
-    formats += [process_text(text, id_token=ID_TOKEN) for text in formats_df["prompt template"].tolist()]
+    formats_df = pd.read_csv(os.path.join(BASE_PATH, "data", "templates", args.bench, f"{task}.csv"))
+    formats += [process_text(text, id_token=True) for text in formats_df["prompt template"].tolist()]
     if task != "word_not_containing":
         n_examples.append(formats_df.samples[0] if not mmlu else n_examples_mmlu[i])
     else:
         # this one does not have the full Ys. Check at some point why.
         n_examples.append(26)
     n_formats.append(len(formats_df))
-
-if args.model_name == "OneHotRaschModel":
-    formats_tokenized = None
-else:
-    formats_tokenized = tokenizer(
-        formats, return_tensors="pt", padding="max_length", truncation=True, max_length=MAX_LENGTH
-    ).to(device)
 
 # Loading data and model splitting
 with open(os.path.join(BASE_PATH, "data", "Ys.pickle"), "rb") as handle:
@@ -112,10 +74,21 @@ used_llms = (
     if TRAIN_SPLIT_LLMS == "even"
     else list(range(1, len(Ys[args.bench][task]), 2))
 )
+n_llms = len(used_llms)
+
+# Initialize model, optimizer, scheduler
+n_example_tokens = sum(n_examples)
+config = config = ModelConfig(base_model=args.model_name, n_examples=n_example_tokens, n_llms=n_llms, d=D, cls=CLS, bias=False)
+model = MultiLabelRaschModel_ID_tokens(config).to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+scheduler = LambdaLR(optimizer, 
+                     lr_lambda=lambda step: (min(step / args.warmup_steps, 1) if step < args.warmup_steps else args.gamma ** (step - args.warmup_steps)),)
+
+# Run tokenization of formats
+formats_tokenized = model.tokenizer(
+        formats, return_tensors="pt", padding="max_length", truncation=True, max_length=MAX_LENGTH).to(device)
 
 # Data splitting
-n_total_examples = sum(n_examples)
-n_llms = len(used_llms)
 train_indices = []
 val_indices = []
 used_Ys = []
@@ -123,7 +96,7 @@ n_previous_examples = 0
 for n_examples_task, n_formats_task, task in zip(n_examples, n_formats, tasks):
     current_range = range(n_previous_examples, n_previous_examples + n_examples_task)
     # init empty tensor with separate labels for each task
-    temp = np.zeros((n_llms, n_formats_task, n_total_examples))
+    temp = np.zeros((n_llms, n_formats_task, n_example_tokens))
     # add labels at corresponding indices
     temp[:, :, current_range] = np.concatenate([np.expand_dims(Ys[args.bench][task][i], 0) for i in used_llms])
 
@@ -152,43 +125,16 @@ val_dataset = TensorDataset(X_val, format_IDs_val, Y_val)
 train_loader = DataLoader(dataset, batch_size=args.bs, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=int(args.bs_val), shuffle=False)
 
-# Initialize model
-if args.model_name != "OneHotRaschModel":
-    model = (
-        MultiLabelRaschModel(args.model_name, n_examples, n_llms, cls=CLS).to(device)
-        if not ID_TOKEN
-        else MultiLabelRaschModel_ID_tokens(args.model_name, n_examples, n_llms, example_token_lookup, cls=CLS).to(
-            device
-        )
-    )
-    model.model.resize_token_embeddings(len(tokenizer))
-else:
-    model = OneHotRaschModel(n_examples, n_llms).to(device)
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-
-if ALT_SCHEDULER:
-    factor = 0.5
-    patience = 3
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=factor, patience=patience)
-else:
-    scheduler = LambdaLR(
-        optimizer,
-        lr_lambda=lambda step: (
-            min(step / args.warmup_steps, 1) if step < args.warmup_steps else args.gamma ** (step - args.warmup_steps)
-        ),
-    )
-
 # Run training
 model, val_losses, val_esterrors = train_model(
     model,
     optimizer,
     train_loader,
     val_loader,
-    formats_tokenized,
+    formats_tokenized=formats_tokenized,
+    args=args,
+    train_split_llms=TRAIN_SPLIT_LLMS,
     scheduler=scheduler,
-    n_epochs=args.n_epochs,
-    print_step=args.print_step,
 )
 
 # Save results
@@ -196,10 +142,11 @@ results_path = os.path.join(BASE_PATH, "results", "results_ft")
 os.makedirs(results_path, exist_ok=True)
 save_name = get_save_name(args, TRAIN_SPLIT_LLMS, "ID_token")
 
-try:
-    torch.save(model.state_dict(), os.path.join(results_path, f"{save_name}.pt"))
-except:  # noqa
-    print("No state_dict for saving")
+model.save_pretrained(os.path.join(results_path, f"{save_name}.pt"))
+
+if args.push_to_hub:  
+    name = f"id_token_{args.model_name.split('-')[0]}_{TRAIN_SPLIT_LLMS}_{args.bench}"  
+    model.push_to_hub(name)
 
 val_losses_df = pd.DataFrame({"val_losses": val_losses, "val_esterrors": val_esterrors})
 val_losses_df.to_csv(os.path.join(results_path, f"{save_name}.csv"))
